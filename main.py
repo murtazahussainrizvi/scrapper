f01de2c6b7da3f2e78c90a028546e3c7c6297a75
S. Murtaza Hussain Rizvi <murtazahussainrizvi@gmail.com>
2023-08-13 22:32:45 +0500

    scrappy removed bs4 work added

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
-# app.py
-
 from flask import Flask, request, jsonify
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from generic_spider import GenericSpider
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
-@app.route('/scrape', methods=['POST'])
+@app.route('/scrape', methods=['GET'])
 def scrape():
+    url = request.args.get('url')
+
+    if not url:
+        return jsonify({'error': 'Missing URL parameter'}), 400
+
     try:
-        data = request.json
-        url = data.get('url')
-        columns = data.get('columns')
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-        if not url or not columns:
-            return jsonify({'error': 'Invalid input data'}), 400
+        scraped_data = []
 
-        process = CrawlerProcess(get_project_settings())
-        process.crawl(GenericSpider, url=url, columns=columns)
-        process.start()
+        # Replace with appropriate selector for your target table
+        table = soup.find('table')
+        if table:
+            for row in table.find_all('tr'):
+                row_data = [cell.get_text() for cell in row.find_all('td')]
+                scraped_data.append(row_data)
 
-        return jsonify({'message': 'Scraping completed successfully'}), 200
+            return jsonify({'data': scraped_data}), 200
+        else:
+            return jsonify({'error': 'Table not found in the webpage'}), 404
 
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': 'Error fetching webpage: ' + str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
