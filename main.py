ae25782ff207fa873b745a315670bb872b9f66bf
SHaikh Muhammad Hammad <hammad447@hotmail.com>
2023-08-13 23:31:47 +0500

    title extraction

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
 
-@app.route('/scrape', methods=['GET'])
+@app.route('/scrape', methods=['Post'])
 def scrape():
-    url = request.args.get('url')
-
+    url = request.json('url')
+    # print(url)
+    classtoscrape = request.json('classname')
+    print(classtoscrape)
     if not url:
         return jsonify({'error': 'Missing URL parameter'}), 400
 
@@ -15,19 +17,17 @@ def scrape():
         response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.content, 'html.parser')
-
-        scraped_data = []
-
-        # Replace with appropriate selector for your target table
-        table = soup.find('table')
-        if table:
-            for row in table.find_all('tr'):
-                row_data = [cell.get_text() for cell in row.find_all('td')]
-                scraped_data.append(row_data)
-
-            return jsonify({'data': scraped_data}), 200
-        else:
-            return jsonify({'error': 'Table not found in the webpage'}), 404
+        #print(soup)
+            # Extract the title from the parsed HTML
+        title = soup.title.text if soup.title else "Title Not Found"
+        print(title)
+        elements_with_class = soup.find_all(class_='classtoscrape')
+        a =[]
+        extracted_text = [a.append(element) for element in elements_with_class]
+        return jsonify({
+            "Title":title,
+            "Quotes":extracted_text
+        }),200
 
     except requests.exceptions.RequestException as e:
         return jsonify({'error': 'Error fetching webpage: ' + str(e)}), 500
