7697a8f25453d769fae95aa10cebb985685f5aa6
S. Murtaza Hussain Rizvi <murtazahussainrizvi@gmail.com>
2023-08-15 01:35:28 +0500

    multiple comma seperated classes as input

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def scrape():
     print("jsonobject ==>",requestJsonObject)
     url= requestJsonObject.get("url")
     classtoextract = requestJsonObject.get("class")
+    values_array = classtoextract.split(',')
+    print("values array=",values_array)
     if not url:   
         return jsonify({'error': 'Missing URL parameter'}), 400
     try:
@@ -18,12 +20,13 @@ def scrape():
         soup = BeautifulSoup(response.content, 'html.parser')
         print("soup==>",soup)
         scraped_data = []
-
-        elements = soup.find_all(class_=classtoextract)
-        for element in elements:
-            scraped_data.append(element.get_text())
+        for i in values_array:
+            elements = soup.find_all(class_=i)
+            for element in elements:
+                scraped_data.append(element.get_text())
+            return_json = jsonify({i:scraped_data})
         # Replace with appropriate selector for your target table
-        return jsonify({'Quotes': scraped_data}), 200
+        return return_json, 200
     except requests.exceptions.RequestException as e:
         return jsonify({'error': 'Error fetching webpage: ' + str(e)}), 500
 
