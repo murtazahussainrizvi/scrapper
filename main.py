5cffe631727398d800cbbfcdc77d6dc1bcd73c46
SHaikh Muhammad Hammad <hammad447@hotmail.com>
2023-08-17 23:25:08 +0500

    nextpage edit

--- main.py
+++ main.py
@@ -11,30 +11,51 @@ def scrape():
     requestJsonObject = request.json
     print("jsonobject ==>",requestJsonObject)
     url= requestJsonObject.get("url")
+    baseurl = url
     classtoextract = requestJsonObject.get("class")
+    nextpagetoextract = requestJsonObject.get("nextpage")
+    print(nextpagetoextract, "-----murtaza kutiya")
     values_array = classtoextract.split(',')
     print("values array=",values_array)
     if not url:   
         return jsonify({'error': 'Missing URL parameter'}), 400
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.content, 'html.parser')
-        #print(soup)
-            # Extract the title from the parsed HTML
-        title = soup.title.text if soup.title else "Title Not Found"
-        print(title)
-        scraped_data = []
         json_data={}
-        # json_data = json.load(jsonObject)
-        for i in values_array:
-            elements = soup.find_all(class_=i)
-            print("elements",elements)
-            for element in elements:
-                scraped_data.append(element.get_text())
-            json_data[i] = scraped_data
-            scraped_data=[]
-            # return_json = jsonify({i:json_data})
+        while url:
+            response = requests.get(url)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.content, 'html.parser')
+            #print(soup)
+                    # Extract the title from the parsed HTML
+            title = soup.title.text if soup.title else "Title Not Found"
+            print(title)
+            scraped_data = []
+        
+                # json_data = json.load(jsonObject)
+            for i in values_array:
+                elements = soup.find_all(class_=i)
+                print("elements",elements)
+                for element in elements:
+                    scraped_data.append(element.get_text())
+                json_data[i] = scraped_data
+                scraped_data=[]
+            try:
+                next_page_link = soup.find("li", class_=nextpagetoextract).find("a")["href"]
+                next_page_url = baseurl + next_page_link
+                print("=====>",next_page_link,next_page_url)
+                if next_page_url==url:
+                    break
+                url = next_page_url
+            except:
+                df = pd.DataFrame(json_data)
+                excel_writer = pd.ExcelWriter('quotes.xlsx', engine='openpyxl')
+                df.to_excel(excel_writer, sheet_name='Quotes', index=False)
+                excel_writer.close()
+                response = make_response(send_file('quotes.xlsx', as_attachment=True, download_name='quotes.xlsx'))
+                response.headers['Content-Type'] = 'application/vnd.ms-excel'
+                return response
+                
+         # return_json = jsonify({i:json_data})
         # Replace with appropriate selector for your target table
         df = pd.DataFrame(json_data)
         excel_writer = pd.ExcelWriter('quotes.xlsx', engine='openpyxl')
@@ -43,9 +64,7 @@ def scrape():
         response = make_response(send_file('quotes.xlsx', as_attachment=True, download_name='quotes.xlsx'))
         response.headers['Content-Type'] = 'application/vnd.ms-excel'
         return response
-        # return json_data, 200
-        
-
+    # return json_data, 
     except requests.exceptions.RequestException as e:
         return jsonify({'error': 'Error fetching webpage: ' + str(e)}), 500
 
