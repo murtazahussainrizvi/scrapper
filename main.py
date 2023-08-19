dddd5493d3503f6bdbc9958f0a67d6864585f580
SHaikh Muhammad Hammad <hammad447@hotmail.com>
2023-08-19 12:23:52 +0500

    finalizing

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ def scrape():
     if not url:   
         return jsonify({'error': 'Missing URL parameter'}), 400
     try:
-        json_data={}
+        json_data={"data":[
+
+        ]}
         while url:
             response = requests.get(url)
             response.raise_for_status()
@@ -37,7 +39,7 @@ def scrape():
                 print("elements",elements)
                 for element in elements:
                     scraped_data.append(element.get_text())
-                json_data[i] = scraped_data
+                json_data["data"].append(scraped_data)
                 scraped_data=[]
             try:
                 next_page_link = soup.find("li", class_=nextpagetoextract).find("a")["href"]
@@ -47,19 +49,21 @@ def scrape():
                     break
                 url = next_page_url
             except:
-                df = pd.DataFrame(json_data)
-                excel_writer = pd.ExcelWriter('quotes.xlsx', engine='openpyxl')
-                df.to_excel(excel_writer, sheet_name='Quotes', index=False)
-                excel_writer.close()
-                response = make_response(send_file('quotes.xlsx', as_attachment=True, download_name='quotes.xlsx'))
-                response.headers['Content-Type'] = 'application/vnd.ms-excel'
-                return response
-                
+                print('The scraper has gone through all the pages')
+                break
          # return_json = jsonify({i:json_data})
         # Replace with appropriate selector for your target table
-        df = pd.DataFrame(json_data)
+        # Extract quotes and authors from the alternating data
+        quotes = json_data["data"][::2]  # Extract even-indexed elements
+        authors = json_data["data"][1::2]  # Extract odd-indexed elements
+
+        # Create a DataFrame
+        df = pd.DataFrame({"Quote": quotes, "Author": authors})
+        # Create a new DataFrame to hold the separated data
+        exploded_df = df.explode(['Quote', 'Author'], ignore_index=True)
+
         excel_writer = pd.ExcelWriter('quotes.xlsx', engine='openpyxl')
-        df.to_excel(excel_writer, sheet_name='Quotes', index=False)
+        exploded_df.to_excel(excel_writer, sheet_name='Quotes', index=False)
         excel_writer.close()
         response = make_response(send_file('quotes.xlsx', as_attachment=True, download_name='quotes.xlsx'))
         response.headers['Content-Type'] = 'application/vnd.ms-excel'
@@ -70,5 +74,3 @@ def scrape():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-    ## COMMENTED FOR PUSHING
